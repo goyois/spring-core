0d55f5d93156b94da63b55820b20c808bba67651
goyois <bau12288@gmail.com>
2023-07-14 18:53:22 +0900

    AppConfig 수정

--- src/main/java/spring/core/config/AppConfig.java
+++ src/main/java/spring/core/config/AppConfig.java
@@ -1,6 +1,8 @@
 package spring.core.config;
 
+import spring.core.discount.DiscountPolicy;
 import spring.core.discount.FixDiscountPolicy;
+import spring.core.discount.RateDiscountPolicy;
 import spring.core.member.Member;
 import spring.core.member.MemberService;
 import spring.core.member.MemberServiceImpl;
@@ -12,11 +14,21 @@ import spring.core.order.OrderServiceImpl;
 public class AppConfig {
 
     //구현체에 의존하지않고 추상화에 의존시켜 결합력을 낮추는 생성자 주입 방법
+    //현재 상태에서는 memberService 에서는 MemberSerivceImpl 를 사용할 것이다.
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(getMemberRepository());  //Extract Method refactor
+    }
+
+    //현재 상태에서는 getMemberRepository 에서는 MemoryMemberRepository 를 사용할 것이다.
+    private static MemoryMemberRepository getMemberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(),new FixDiscountPolicy());
+        return new OrderServiceImpl(getMemberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
     }
 }
